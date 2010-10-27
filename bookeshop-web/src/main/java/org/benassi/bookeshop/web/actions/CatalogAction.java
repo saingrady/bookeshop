092cc9584714b17a4acb335d1f36afd4d65b8756
benas <md.benhassine@gmail.com>
2010-10-27 20:23:16 +0000

    add struts CatalogAction

---
+++ bookeshop-web/src/main/java/org/benassi/bookeshop/web/actions/CatalogAction.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the Book-eShop project.
+ *
+ *  Copyright (C) 2010-2011 Mahmoud Ben Hassine <md.benhassine@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Author :
+ * 	Mahmoud Ben Hassine <md.benhassine@gmail.com>
+ */
+
+package org.benassi.bookeshop.web.actions;
+
+import fr.mbh.bookeshop.business.api.BookManager;
+import fr.mbh.bookeshop.dao.domain.Book;
+
+import java.util.List;
+
+
+public class CatalogAction {
+
+    private BookManager bookManager;
+
+    public void setBookManager(BookManager bookManager) {
+        this.bookManager = bookManager;
+    }
+
+    private List<Book> books;
+
+    public List<Book> getBooks() {
+        return bookManager.getBooks();
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public String execute() throws Exception {
+        return "success";
+    }
+
+
+}
