9230cefca8f2257bcd45eb027b9a73b89e55cfd3
benas <md.benhassine@gmail.com>
2010-11-07 21:07:33 +0000

    add logout action

---
+++ bookeshop-web/src/main/java/org/benassi/bookeshop/web/actions/LogoutAction.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Book-eShop project.
+ *
+ *  Copyright (C) 2010-2011 Mahmoud Ben Hassine <md.benhassine@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Author :
+ * 	Mahmoud Ben Hassine <md.benhassine@gmail.com>
+ */
+
+package org.benassi.bookeshop.web.actions;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.util.Map;
+
+/**
+ * Action class to logout a customer
+ */
+public class LogoutAction implements SessionAware {
+
+    private Map<String, Object> session;
+
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
+
+    public String execute() {
+        if (session instanceof org.apache.struts2.dispatcher.SessionMap) {
+            try {
+                ((org.apache.struts2.dispatcher.SessionMap) session).invalidate();
+            } catch (IllegalStateException e) {
+                // TODO add log
+                // return "error";
+            }
+        }
+        return "success";
+    }
+}
