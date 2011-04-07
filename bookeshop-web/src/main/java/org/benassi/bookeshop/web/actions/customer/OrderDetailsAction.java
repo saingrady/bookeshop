6f8bae2cd1185a847b8dcaa155dc1153cef7e2c2
benas <md.benhassine@gmail.com>
2011-04-07 22:11:33 +0000

    add action to get order details

---
+++ bookeshop-web/src/main/java/org/benassi/bookeshop/web/actions/customer/OrderDetailsAction.java
@@ -0,0 +1,80 @@
+/*
+ * This file is part of the Book-eShop project.
+ *
+ *    Copyright (C) 2010-2011 Mahmoud Ben Hassine <md.benhassine@gmail.com>
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ *    Author :
+ *   	Mahmoud Ben Hassine <md.benhassine@gmail.com>
+ */
+
+package org.benassi.bookeshop.web.actions.customer;
+
+import fr.mbh.bookeshop.business.api.BookManager;
+import fr.mbh.bookeshop.business.api.OrderManager;
+import org.benassi.bookeshop.data.model.Book;
+import org.benassi.bookeshop.data.model.Order;
+import org.benassi.bookeshop.data.model.OrderItem;
+
+import java.util.Set;
+
+/**
+ * BookDetailsAction : action class to get book details
+ */
+public class OrderDetailsAction {
+
+    private OrderManager orderManager;
+
+    private int orderId;
+
+    private Order order;
+
+    private Set<OrderItem> items;
+
+    private String error;
+
+    public void setOrderManager(OrderManager orderManager) {
+        this.orderManager = orderManager;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String execute(){
+        order = orderManager.getOrderById(orderId);
+        items = order.getItems();
+        if (order != null)
+            return "success";
+        else {
+            error = "No such order with ID = " + orderId;
+            return "error";
+        }
+
+    }
+}
---
+++ bookeshop-web/src/main/webapp/WEB-INF/jsp/order-details.jsp
@@ -0,0 +1,50 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="display" uri="http://displaytag.sf.net/el"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<%--
+  ~ This file is part of the Book-eShop project.
+  ~
+  ~  Copyright (C) 2010-2011 Mahmoud Ben Hassine <md.benhassine@gmail.com>
+  ~
+  ~  This program is free software; you can redistribute it and/or modify
+  ~  it under the terms of the GNU General Public License as published by
+  ~  the Free Software Foundation; either version 2 of the License, or
+  ~  (at your option) any later version.
+  ~
+  ~  This program is distributed in the hope that it will be useful,
+  ~  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ~  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ~  GNU General Public License for more details.
+  ~
+  ~  You should have received a copy of the GNU General Public License
+  ~  along with this program; if not, write to the Free Software
+  ~  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+  ~
+  ~ Author :
+  ~ 	Mahmoud Ben Hassine <md.benhassine@gmail.com>
+  --%>
+
+<jsp:include page="header.jsp" flush="true"/>
+<jsp:include page="left-sidebar.jsp" flush="true"/>
+<div id="content">
+    <div class="post">
+        <div class="post-bgtop">
+            <div class="post-bgbtm">
+                <div align="center">
+                    <h1> Order details : ${order.orderId} - ${order.formattedDate} (${order.status.label})  </h1><br/>
+                    <display:table name="items" uid="item" sort="list" defaultorder="descending" requestURI="orderDetails.do">
+                        <display:column property="bookId" title="Book ID"/>
+                        <display:column property="quantity" title="Quantity" sortable="true"/>
+                    </display:table>
+                </div>
+                <div style="clear: both;">&nbsp;</div>
+            </div>
+        </div>
+    </div>
+    <div style="clear: both;">&nbsp;</div>
+</div>
+<!-- end #content -->
+<jsp:include page="right-sidebar.jsp" flush="true"/>
+<jsp:include page="footer.jsp" flush="true"/>               
+
