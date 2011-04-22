20c9e87f440dbcce3e2d976c923c9b6ffed2c18d
benas <md.benhassine@gmail.com>
2011-04-22 22:18:19 +0000

    fix/refactor download catalogue action

--- bookeshop-web/src/main/java/org/benassi/bookeshop/web/actions/DownloadCatalogueAction.java
+++ bookeshop-web/src/main/java/org/benassi/bookeshop/web/actions/DownloadCatalogueAction.java
@@ -23,42 +23,28 @@
 
 package org.benassi.bookeshop.web.actions;
 
-import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
-import org.apache.struts2.interceptor.ServletResponseAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.core.io.Resource;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.benassi.bookeshop.web.util.PdfCatalogueGenerator;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.InputStream;
 
 /**
  * Action class to generate and download PDF catalogue
  */
-public class DownloadCatalogueAction implements ServletResponseAware, ApplicationContextAware {
+public class DownloadCatalogueAction  {
 
-    private ApplicationContext applicationContext;
+    private PdfCatalogueGenerator pdfCatalogueGenerator;
 
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
-
-    private HttpServletResponse response;
-
-     public void setServletResponse(HttpServletResponse response){
-       this.response = response;
-     }
-
-     public HttpServletResponse getServletResponse(){
-       return response;
-     }
+    private InputStream inputStream;
 
     private String error;
+
+    public void setPdfCatalogueGenerator(PdfCatalogueGenerator pdfCatalogueGenerator) {
+        this.pdfCatalogueGenerator = pdfCatalogueGenerator;
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
 
     public String getError() {
         return error;
@@ -66,77 +52,14 @@ public class DownloadCatalogueAction implements ServletResponseAware, Applicatio
 
     public String execute(){
 
-        //TODO externalize configuration
-        
-        Resource jreport = applicationContext.getResource("classpath:reports/report.jrxml");
+        inputStream = pdfCatalogueGenerator.getPdfCatalogueStream();
 
-
-        byte[] bytes = null;
-        java.sql.Connection connection = null;
-        java.sql.Statement statement = null;
-        ServletOutputStream servletOutputStream = null;
-        try {
-            servletOutputStream = response.getOutputStream();
-            Class.forName("org.hsqldb.jdbcDriver").newInstance();
-            connection = DriverManager.getConnection("jdbc:hsqldb:mem:bookeshop", "SA", "");
-            statement = connection.createStatement();
-            String query = "SELECT title,author,price FROM book;";
-            java.sql.ResultSet resultSet = statement.executeQuery(query);
-
-            //Certainly not in production environment!
-            //Jasper reports should be compiled only once
-            JasperDesign jasperDesign = JRXmlLoader.load(jreport.getFile());
-            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, new JRResultSetDataSource(resultSet));
-
-            bytes = JasperExportManager.exportReportToPdf(jasperPrint);
-
-            response.setContentType("application/pdf");
-            response.setContentLength(bytes.length);
-            // TODO check file extension for downloaded file (currently .do!)
-
-            servletOutputStream.write(bytes, 0, bytes.length);
-            servletOutputStream.flush();
-            servletOutputStream.close();
-
+        if (inputStream != null)
             return"success";
-
-
-        }
-        catch (Exception e) {
-            e.printStackTrace();
+        else{
             error = "An exception occurred while generating PDF catalog.";
             return "error";
-        }finally {
-            try {
-                statement.close();
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
         }
-
-        /* May be used if PDF catalog is already prepared (static content) and not generated on the fly with Jasper Reports
-
-        Resource catalog = applicationContext.getResource("classpath:catalog/catalog.pdf");
-        try {
-            InputStream is = catalog.getInputStream();
-            OutputStream os = response.getOutputStream();
-            response.setContentType("application/pdf");
-            response.setHeader("Content-Disposition", "attachment;filename=catalog.pdf");
-            int count;
-            byte buf[] = new byte[4096];
-            while ((count = is.read(buf)) > -1) {
-                os.write(buf, 0, count);
-            }
-            is.close();
-            os.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
 
     }
 }
