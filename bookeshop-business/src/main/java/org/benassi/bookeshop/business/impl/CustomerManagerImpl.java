d1a49888db40e962eb66dd2e1d649bf1a84af9b7
benas <md.benhassine@gmail.com>
2011-06-19 19:58:31 +0000

    remove customer orders before removing the customer (since foreign keys were removed)

--- bookeshop-business/src/main/java/org/benassi/bookeshop/business/impl/CustomerManagerImpl.java
+++ bookeshop-business/src/main/java/org/benassi/bookeshop/business/impl/CustomerManagerImpl.java
@@ -28,13 +28,17 @@ import org.benassi.bookeshop.business.api.CustomerManager;
 import org.benassi.bookeshop.business.exception.CustomerExistentException;
 import org.benassi.bookeshop.business.exception.LoginException;
 import org.benassi.bookeshop.data.access.api.CustomerDAO;
+import org.benassi.bookeshop.data.access.api.OrderDAO;
 import org.benassi.bookeshop.data.model.Customer;
+import org.benassi.bookeshop.data.model.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.dao.DataAccessException;
 
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 /**
  * Customer Manager implementation
@@ -45,10 +49,16 @@ public class CustomerManagerImpl implements CustomerManager {
 
     private CustomerDAO customerDAO;
 
+    private OrderDAO orderDAO;
+
     private MessageSource messages;
 
     public void setCustomerDAO(CustomerDAO customerDAO) {
         this.customerDAO = customerDAO;
+    }
+
+    public void setOrderDAO(OrderDAO orderDAO) {
+        this.orderDAO = orderDAO;
     }
 
     public void setMessages(MessageSource messages) {
@@ -76,7 +86,11 @@ public class CustomerManagerImpl implements CustomerManager {
     }
 
     public void removeCustomer(Customer customer) {
-            customerDAO.delete(customer);
+        Set<Order> orders = orderDAO.getOrdersByCustomer(customer.getId());
+        for(Order order : orders){
+            orderDAO.removeOrder(order);
+        }
+        customerDAO.delete(customer);
     }
 
     public Customer createCustomer(Customer customer) throws CustomerExistentException {
@@ -84,7 +98,7 @@ public class CustomerManagerImpl implements CustomerManager {
             customerDAO.save(customer);
             return customerDAO.getCustomerById(customer.getId());
         } catch (DataAccessException e) {
-            logger.error("Error updating customer [" + customer + "]",e);
+            logger.error("Error creating customer [" + customer + "]",e);
             throw new CustomerExistentException();
         }
     }
