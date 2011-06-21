df2153e4314b747be8821ad87fc6db551fdabe06
benas <md.benhassine@gmail.com>
2011-06-21 20:09:52 +0000

    simplify data model constraints

--- bookeshop-web/src/main/resources/config/schema.sql
+++ bookeshop-web/src/main/resources/config/schema.sql
@@ -64,7 +64,6 @@ CREATE TABLE book_order (
   statusId int NOT NULL
 );
 
--- alter table book_order add constraint customer_fk foreign key (customerId) references customer(id);
 alter table book_order add constraint status_fk foreign key (statusId) references book_order_status(id);
 
 -- Order item table
@@ -78,5 +77,4 @@ CREATE TABLE book_order_item (
   purchasePrice float NOT NULL
 );
 
--- alter table book_order_item add constraint order_id_fk foreign key (orderId) references book_order(orderId);
 alter table book_order_item add constraint book_id_fk foreign key (bookId) references book(isbn);
